989b83fd5729bcb3c7fd12381145b16f510fd52d
Florian Nitschmann <f.nitschmann@googlemail.com>
2017-07-28 13:32:20 +0200

    changed handling of environment settings

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from config import ApplicationConfig
 from controllers.api import ApiController
 
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
-environment = os.getenv("ENVIRONMENT", "development")
+environment = os.environ.get("ENV", os.getenv("ENV", "development"))
 application_config = ApplicationConfig.get_instance(environment=environment)
 
 # Flask application
@@ -17,6 +17,9 @@ app.config["APPLICATION_CONFIG"] = application_config
 app.config["ORATOR_DATABASES"] = {
         environment: application_config.database_config()
         }
+
+if environment is not "production":
+    app.config["ORATOR_DATABASES"]["test"] = application_config.database_config_for_env("test")
 
 # Orator db
 db = Orator(app)
--- config/application.py
+++ config/application.py
@@ -7,25 +7,36 @@ if p not in sys.path:
 from utils import Singleton
 
 class ApplicationConfig(Singleton):
-    ALLOWED_ENVS = ["development", "production"]
+    ALLOWED_ENVS = ["development", "production", "test"]
 
     def __init__(self, environment = "development"):
-        if environment in self.ALLOWED_ENVS:
-            self.environment = environment
-        else:
-            err_msg = "environment has to be one of {}".format(self.ALLOWED_ENVS)
-            raise ValueError(err_msg)
+        self.set_env(environment)
 
     def database_config(self):
+        return self.database_config_for_env(self.environment)
+
+    def database_config_for_env(self, environment):
         config = {}
         config_filepath = os.path.abspath(os.path.join(os.path.dirname(__file__),
             "database.yml"))
 
         with open(config_filepath, "r") as config_file:
             config = yaml.load(config_file)
-            config = config[self.environment]
+            config = config[environment]
 
         return config
 
     def debug_mode_is_enabled(self):
-        return self.environment == "development"
+        return self.environment == "development" or self.environment == "test"
+
+    def set_env(self, environment):
+        """
+        NOTE: This function should never be called in production environments
+        and be treated with care
+        """
+        if environment in self.ALLOWED_ENVS:
+            self.environment = environment
+        else:
+            err_msg = "environment has to be one of {}".format(self.ALLOWED_ENVS)
+            raise ValueError(err_msg)
+
